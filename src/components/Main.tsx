3358119ce8b81271fdbd15f2ca253689006f7341
Firdaus <muhdnorfirdaus27@gmail.com>
2024-07-21 21:52:47 +0800

    fix

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,11 +6,9 @@ type MainProps = {
 
 const Main: React.FC<MainProps> = ({ children }) => {
   return (
-    <>
       <div role="main" className="main transformZ0">
         {children}
       </div>
-    </>
   );
 };
 
